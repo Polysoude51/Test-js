5affd1be479a7fe2a3278ef037efc1d6a918e161
Polysoude51 <q.ducoin@polysoude.com>
2024-12-04 12:03:14 +0100

    function javascript

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-var js=document.querySelector('#js-notification')
-js.style.backgroundColor='green'
-js.innerHTML='le <span style=font-weight:900;color:blue;>js</span> est OK'
-
-if(confirm("Le fichier js est chargé")){
-    console.log("c\'est ok");
+function loadingDOM(){
+    var js=document.querySelector('#js-notification');
+    js.style.backgroundColor='green';
+    js.innerHTML='le <span style=font-weight:900;color:blue;>js</span> est OK';
 }
+
+document.addEventListener('DOMContentLoaded',loadingDOM);
