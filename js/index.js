a0b4641f3957b0cb89be2401b4ed3077b9e18b46
Polysoude51 <q.ducoin@polysoude.com>
2024-12-04 12:22:28 +0100

    evenetment propagation

--- js/index.js
+++ js/index.js
@@ -1,7 +1,18 @@
-function loadingDOM(){
-    var js=document.querySelector('#js-notification');
-    js.style.backgroundColor='green';
-    js.innerHTML='le <span style=font-weight:900;color:blue;>js</span> est OK';
+function loadingDOM() {
+  var js = document.querySelector("#js-notification");
+  js.style.backgroundColor = "green";
+  js.innerHTML = "le <span style=font-weight:900;color:blue;>js</span> est OK";
+  document
+    .querySelector("button.btn-danger")
+    .addEventListener("click", function (evt) {
+    evt.stopPropagation();
+      console.log(evt);
+      console.log("boutton du header clické");
+    });
+  document.querySelector("#header").addEventListener("click", function (evt) {
+    console.log("header", evt);
+    console.log("header clické");
+  });
 }
 
-document.addEventListener('DOMContentLoaded',loadingDOM);
+document.addEventListener("DOMContentLoaded", loadingDOM);
