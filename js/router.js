2e2320b5ed44b521fcb6e6e9a12e138284d44c44
Polysoude51 <q.ducoin@polysoude.com>
2024-12-04 17:01:45 +0100

    function router

---
+++ js/router.js
@@ -0,0 +1,16 @@
+/*
+public->sur this
+    +page actuelle champs lecture
+    +changement de chemin fonction
+privés
+    +champs de route prive en ecriture
+    +moddif url
+    +moddif contenu (loadEvants)
+    +recuperer du conteu de page depuis le reseau
+*/
+function Router() {
+  var currentRoute = location.pathname;
+  function changePathName(pathname) {}
+  function loadContentInPage(eventLoader) {}
+  function getContentFromNetwork(contentUrl) {}
+}
