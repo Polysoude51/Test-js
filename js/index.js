a39093778109574184c3c495eff89184a9fec8e4
Polysoude51 <q.ducoin@polysoude.com>
2024-12-04 11:06:22 +0100

    js commit modification js_notif

--- js/index.js
+++ js/index.js
@@ -1,3 +1,7 @@
+var js=document.querySelector('#js-notification')
+js.style.backgroundColor='green'
+js.innerHTML='le <span style=font-weight:900;color:blue;>js</span> est OK'
+
 if(confirm("Le fichier js est chargé")){
     console.log("c\'est ok");
 }
