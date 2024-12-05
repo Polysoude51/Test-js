24647ce35d01e3cf7466576422fc1cb0b53b2bd2
Polysoude51 <q.ducoin@polysoude.com>
2024-12-05 16:22:35 +0100

    fetch meme

--- js/metier/meme.js
+++ js/metier/meme.js
@@ -1,28 +1,51 @@
+/**
+ * class de gestion d'un meme
+ */
 class Meme {
-  id = undefined;
-  text = "";
-  x = 0;
-  y = 10;
-  fontsize = 10;
-  fontweight = 500;
-  underline = false;
-  italic = false;
-  color = "#FFFFFF";
-  imageId = -1;
-  #endpoint = "/memes";
-  constructor() {}
-  save() {
-    console.log("save at" +this.id + this.#endpoint, this);
-    this.publicSave();
-    this.#privateSave();
+    id = undefined;
+    text = "";
+    x = 0;
+    y = 10;
+    fontSize = 10;
+    fontWeight = 500;
+    underline = false;
+    italic = false;
+    color = "#FFFFFF";
+    imageId = -1;
+    #endpoint = "/memes";
+    /**
+     * constructor de meme
+     */
+    constructor() {
+      console.log("constructor de meme");
+    }
+    save() {
+      fetch(
+        `http://localhost:5679${this.#endpoint}${
+          undefined !== this.id ? "/" + this.id : ""
+        }`,
+        {
+          method: undefined !== this.id ? "PUT" : "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(this),
+        }
+      )
+        .then((r) => r.json())
+        .then((o) => {
+          Object.assign(this, o);
+        });
+      // console.log("save " + this.id + " at " + this.#endpoint, this);
+      // this.publicSave();
+      // this.#privateSave();
+    }
+    publicSave() {
+      console.log("public saving");
+    }
+    #privateSave() {
+      console.log("private saving");
+    }
   }
-  publicSave() {
-    console.log("public saving");
-  }
-
-  #privateSave() {
-    console.log("private saving");
-  }
-}
-let meme = new Meme();
-meme.save();
+  let meme = new Meme();
+  //meme.save();
