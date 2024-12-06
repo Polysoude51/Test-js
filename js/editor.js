0a59fd474dab2f33f8ab909fe49cfa397aca5eb8
Polysoude51 <q.ducoin@polysoude.com>
2024-12-06 11:44:22 +0100

    maj

--- js/editor.js
+++ js/editor.js
@@ -1,16 +1,66 @@
+let currentMeme = new Meme();
+let editorRootSvg = undefined;
 function loadEditor(params) {
+  editorRootSvg = document.querySelector("#wrapper svg");
   console.log(params);
   loadEditorEvent();
-  promiseImage.then((arrayImages) => {
+  promiseImages.then((arrayImages) => {
     loadSelectImagesInForm(arrayImages);
   });
 }
-
+function treatInputStringEventChange(evt) {
+  currentMeme[evt.target.name] = evt.target.value;
+  updateSVG(currentMeme, editorRootSvg);
+}
+function treatInputNumberEventChange(evt) {
+  currentMeme[evt.target.name] = parseInt(evt.target.value);
+  updateSVG(currentMeme, editorRootSvg);
+}
+function treatCheckEventChange(evt) {
+  currentMeme[evt.target.name] = evt.target.checked;
+  updateSVG(currentMeme, editorRootSvg);
+}
 function loadEditorEvent() {
   document.forms["editor-form"].addEventListener("submit", function (evt) {
     evt.preventDefault();
-    console.log("form submit");
+    currentMeme.save();
   });
+  document.forms["editor-form"]["text"].addEventListener(
+    "input",
+    treatInputStringEventChange
+  );
+  document.forms["editor-form"]["imageId"].addEventListener(
+    "change",
+    treatInputNumberEventChange
+  );
+  document.forms["editor-form"]["x"].addEventListener(
+    "change",
+    treatInputNumberEventChange
+  );
+  document.forms["editor-form"]["y"].addEventListener(
+    "change",
+    treatInputNumberEventChange
+  );
+  document.forms["editor-form"]["color"].addEventListener(
+    "change",
+    treatInputStringEventChange
+  );
+  document.forms["editor-form"]["fontSize"].addEventListener(
+    "change",
+    treatInputNumberEventChange
+  );
+  document.forms["editor-form"]["fontWeight"].addEventListener(
+    "change",
+    treatInputStringEventChange
+  );
+  document.forms["editor-form"]["underline"].addEventListener(
+    "change",
+    treatCheckEventChange
+  );
+  document.forms["editor-form"]["italic"].addEventListener(
+    "change",
+    treatCheckEventChange
+  );
 }
 /**
  *
@@ -28,4 +78,49 @@ const loadSelectImagesInForm = (images) => {
     select.appendChild(optionClone);
   });
   // debugger;
+};
+/**
+ *  construction interne d'une balise svg pour affichage de meme
+ * @param {Meme} meme meme a afficher
+ * @param {HTMLElement} svgRootNode noeud svg pour affichage
+ */
+const updateSVG = (meme, svgRootNode) => {
+  const img = images.find((i) => {
+    return i.id === meme.imageId;
+  });
+  //1. svg viewbox
+  svgRootNode.setAttribute(
+    "viewBox",
+    `0 0 ${undefined !== img ? img.w : "500"} ${
+      undefined !== img ? img.h : "500"
+    }`
+  );
+  svgRootNode.innerHTML = "";
+  //2.selection a partir de svgRootNode -> text
+  const text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  text.innerHTML = meme.text;
+  //mise a jour des attributs de text
+  text.setAttribute("x", meme.x);
+  text.setAttribute("y", meme.y);
+  text.setAttribute("font-size", meme.fontSize);
+  text.setAttribute("font-weight", meme.fontWeight);
+  text.setAttribute("fill", meme.color);
+  text.setAttribute("text-decoration", meme.underline ? "underline" : "none");
+  text.setAttribute("font-style", meme.italic ? "italic" : "normal");
+  svgRootNode.appendChild(text);
+
+  if (undefined !== img) {
+    const imageSVG = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "image"
+    );
+    imageSVG.setAttribute("x", 0);
+    imageSVG.setAttribute("y", 0);
+    /*const href=document.createAttributeNS("http://www.w3.org/1999/xlink",'xlink:href');
+    href.value=img.url;
+    imageSVG.setAttributeNodeNS(href);*/
+    imageSVG.setAttributeNS("http://www.w3.org/1999/xlink","xlink:href", img.url);
+
+    svgRootNode.insertBefore(imageSVG, text);
+  }
 };
